d1746808039df89446f7326073966321aa348e87
johnnynunez <johnnynuca14@gmail.com>
2023-11-26 12:52:17 +0100

    .

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 import setuptools
+try:
+   import pypandoc
+   long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+   long_description = open('README.md').read()
 
 setuptools.setup(
     scripts=['bin/ai-benchmark'],
+    long_description_content_type="text/markdown",
+
+    long_description=README,
     packages=setuptools.find_packages(),
 )
